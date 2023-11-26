ff1c38dc22296c17d799934aadc6653939427d7a
minibot47 <toludairo534@gmail.com>
2023-11-26 18:23:23 +0100

    done with my pages

--- my-chainmine-app/src/App.js
+++ my-chainmine-app/src/App.js
@@ -10,28 +10,10 @@ import Affiliate from './Affiliate page/Affiliate';
 import Insights from './Insights page/Insights';
 import About from './AboutPage/About';
 import Footer from './Footer/Footer'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Faqs from './FAQS/Faqs'
+import Terms from './Terms/Terms'
 
-=======
-import Faqs from './FAQS/Faqs'
-import Terms from './Terms/Terms'
->>>>>>> Stashed changes
-=======
-import Faqs from './FAQS/Faqs'
-import Terms from './Terms/Terms'
->>>>>>> Stashed changes
-=======
-import Faqs from './FAQS/Faqs'
-import Terms from './Terms/Terms'
->>>>>>> Stashed changes
-=======
-import Faqs from './FAQS/Faqs'
-import Terms from './Terms/Terms'
->>>>>>> Stashed changes
 
 
 function App() {
@@ -46,28 +28,16 @@ function App() {
       <Route element={<Pricing/>} path='/Pricing'/>
       <Route element={<Forgot/>} path='/Forgot' />
     {/* <Hompage/> */}
-<<<<<<< Updated upstream
     <Route element={<Affiliate/>} path='/Affiliate'/>
     <Route element={<Insights/>} path='/Insights'/>
     <Route element={<About/>} path='/About'/>
     </Routes>
-=======
     {/* <Pricing/> */}
     {/* <Affiliate/> */}
     {/* <Insights/> */}
     {/* <About/> */}
     {/* <Faqs/> */}
     <Terms/>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     <Footer/>
     
     </BrowserRouter>
